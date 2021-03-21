0241898c29f9de840dc08769f95223ef0010795e
EldersJavas <55266266+EldersJavas@users.noreply.github.com>
2021-03-21 22:06:56 +0800

    Update

--- class/HTProject.class.py
+++ class/HTProject.class.py
@@ -1,46 +1,45 @@
-import os
-test
-def test(h):
-    h=HTProject(True)    
-    
+
+
 
 class HTProject :
     IsNew=False
     File=''
     #def __init__(self):
+
+    
+ #   def Crate(self,con) :   
+#        Item[con]=self.
+                     
+    ProjectName=''
+    ResoureLanguage=''
+    Author=[]
+    Member=[]
+    Contributor=[]
+    HTVersion='1.0'
+    PVersion=''
+    BuildTime=''
+    LastChange=''
+    License=''
+    describtion=''
+    Website=''
+    HTsite=''
+    ProjectLocal='..'
+    IsClock=False
+    IsFinish=False
+    NeedCheck=True
+        
+#    def __init__(self,IsNew,ProjectName, ResoureLanguage, Author,Member,Contributor,HTVersion,PVersion, BuildTime, LastChange, License, describtion, Website, HTsite, ProjectLocal,IsClock,IsFinish,NeedCheck) :
+ #       if (IsNew) :
+  #          self.ProjectName=ProjectName
+   #         self.ResoureLanguage=ResourceLanguage
+    #        
+#
+ #       else :
+  #          open()
+
     def __init__(self,IsNew):
         if (IsNew):
             self.IsNew=IsNew
-
-    #def Crate(self,con) :   
-        #con=self.ProjectConfig(ProjectName)
-                     
-    class ProjectConfig :
-
-        ProjectName=''
-        ResoureLanguage=''
-        Author=[]
-        Member=[]
-        Contributor=[]
-        HTVersion='1.0'
-        PVersion=''
-        BuildTime=''
-        LastChange=''
-        License=''
-        describtion=''
-        Website=''
-        HTsite=''
-        ProjectLocal='..'
-        IsClock=False
-        IsFinish=False
-        NeedCheck=True
-        
-        def __init__(self,IsNew,ProjectName, ResoureLanguage, Author,Member,Contributor,HTVersion,PVersion, BuildTime, LastChange, License, describtion, Website, HTsite, ProjectLocal,IsClock,IsFinish,NeedCheck) :
-            if (IsNew) :
-                self.ProjectName=ProjectName
-            else :
-                open()
-
 
     class TranItem :
         
@@ -55,7 +54,7 @@ class HTProject :
 
 
         #def __init__() :
-        
+            
     #def __init__() :
     class Rule :
         ReaderRuleFile=''
@@ -63,4 +62,12 @@ class HTProject :
         CustomFile=''
         ReaderRule=[]
         WriterRule=[]
-        Custom={ 'func':[] , 'string':[] }
+        Custom={ 
+            'func':[] ,
+            'string':[] 
+                }
+
+
+h=HTProject(True)
+
+print(h.__dict__)
